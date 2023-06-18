b9ae2ff905fbec5685a322f088880bc03ad07e20
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-18 14:55:17 +0530

    Add styles

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Home.scss"
 import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
 
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured />
+      <TrustedBy />
       
     </div>
   )
