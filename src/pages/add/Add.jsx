bebb405f531ea71f24ad621782ac57eb0a68c83b
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-20 22:00:23 +0530

    Updated gig page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -1,64 +1,54 @@
-import React from 'react'
-import "./Add.scss"
+import React from "react";
+import "./Add.scss";
 
 const Add = () => {
   return (
-    <div className='add'>
+    <div className="add">
       <div className="container">
         <h1>Add New Gig</h1>
         <div className="sections">
-          <div className='info'>
-          <div className="left">
-            <label htmlFor ="">Title</label>
-            <input type="text" placeholder="eg. I will do something I'm really good at" />
-
-            <label htmlFor ="">Category</label>
-            <select name ="cats" id="cats">
+          <div className="info">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g. I will do something I'm really good at"
+            />
+            <label htmlFor="">Category</label>
+            <select name="cats" id="cats">
               <option value="design">Design</option>
               <option value="web">Web Development</option>
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
-
-              <label htmlFor ="">Cover Image</label>
-              <input type="file" />
-              <label htmlFor ="">Upload Images</label>
-              <input type="file" multiple />
-              <label htmlFor ="">Description</label>
-              <textarea name="" 
-                placeholder="Brief description about your gig" id="" 
-                cols="30" rows="16"></textarea>
-                <button>Create</button>
-            
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+            <label htmlFor="">Upload Images</label>
+            <input type="file" multiple />
+            <label htmlFor="">Description</label>
+            <textarea name="" id="" placeholder="Brief descriptions to introduce your service to customers" cols="0" rows="16"></textarea>
+            <button>Create</button>
           </div>
-
           <div className="details">
-            <label htmlFor ="">Service Title</label>
-            <input type="text" placeholder="eg. One page web design" />
-            <label htmlFor ="">Short Description</label>
-            <textarea name="" 
-                placeholder="Short description about your gig" id="" 
-                cols="30" rows="10"></textarea>
-            <label htmlFor ="">Delivery Time (eg. 3 days )</label>
+            <label htmlFor="">Service Title</label>
+            <input type="text" placeholder="e.g. One-page web design" />
+            <label htmlFor="">Short Description</label>
+            <textarea name="" id="" placeholder="Short description of your service" cols="30" rows="10"></textarea>
+            <label htmlFor="">Delivery Time (e.g. 3 days)</label>
             <input type="number" />
-            <label htmlFor ="">Revision Time</label>
+            <label htmlFor="">Revision Number</label>
             <input type="number" />
-            <label htmlFor ="">Add feature</label>
-            <input type="text" placeholder="eg. page design" />
-            <input type="text" placeholder="eg. file uploading" />
-            <input type="text" placeholder="eg. setting up a domain" />
-            <input type="text" placeholder="eg. hosting" />
-            <label htmlFor ="">Price</label>
+            <label htmlFor="">Add Features</label>
+            <input type="text" placeholder="e.g. page design" />
+            <input type="text" placeholder="e.g. file uploading" />
+            <input type="text" placeholder="e.g. setting up a domain" />
+            <input type="text" placeholder="e.g. hosting" />
+            <label htmlFor="">Price</label>
             <input type="number" />
-
-          </div>
           </div>
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Add;
-
