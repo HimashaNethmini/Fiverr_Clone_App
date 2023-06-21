51859203be2073b0f8f2a7dd32605703db8a6312
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-21 17:18:25 +0530

    Setup routes files

---
+++ api/routes/user.route.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+//req-what comes from client and res- what we send to them(response)
+
+
+
+
+
+
+
+
+
+
+
+
+export default router;
