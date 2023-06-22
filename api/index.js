49f930dbe495c3890619e939b0f6ffdc0719d8d8
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-22 19:25:31 +0530

    Add cookie parser

--- api/index.js
+++ api/index.js
@@ -1,42 +1,43 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.route.js";
-import ConversationRoute from "./routes/conversation.route.js";
-import GigRoute from "./routes/gig.route.js";
-import MessageRoute from "./routes/message.route.js";
-import OrderRoute from "./routes/order.route.js";
-import ReviewRoute from "./routes/review.route.js";
-import AuthRoute from "./routes/auth.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import gigRoute from "./routes/gig.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 
 const app =express();
 dotenv.config();
+
 // db connection
 const connect = async() =>{
-try{
-    await mongoose.connect(process.env.MONGO);
-console.log("Connected to Mongo DB");
-}catch (error) {
-    console.log(error);
-}
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to Mongo DB");
+    }catch (error) {
+        console.log(error);
+    }
 
-}
-app.use("/api/auth",AuthRoute);
+};
+
+app.use(express.json());
+app.use(cookieParser()); //to authorize n authenticate users
+
+app.use("/api/auth",authRoute);
 app.use("/api/users", userRoute);
-app.use("/api/gigs",GigRoute);
-app.use("/api/messages",MessageRoute);
-app.use("/api/conversations",ConversationRoute);
-app.use("/api/orders",OrderRoute);
-app.use("/api/reviews",ReviewRoute);
-
-
-
-
-
+app.use("/api/gigs",gigRoute);
+app.use("/api/messages",messageRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/orders",orderRoute);
+app.use("/api/reviews",reviewRoute);
 
 
 app.listen(8800, ()=>{
     connect();
     console.log("Backend Server is running");
-})
+});
