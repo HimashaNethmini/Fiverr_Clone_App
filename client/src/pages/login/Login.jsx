17c33af242047de261e92585663547d2c0182bb6
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-23 09:07:01 +0530

    Add login function

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./Login.scss"
+import { useNavigate } from "react-router-dom";
+//import newRequest from "../../utils/newRequest";
 
-const Login = () => {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const res = await newRequest.post("/auth/login", { username, password });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/")
+    } catch (err){
+      setError(err.response.data);
+    }
+  };
+
   return (
-    <div>
-      
+    <div className="login">
+      <form onSubmit = {handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input 
+          name="username"
+          type="text"
+          placeholder='Himasha Nethmini' 
+          onChange={(e) => setUsername(e.target.value)}
+          />
+          
+      </form>
     </div>
   )
 }
+
+
+
 
 export default Login
